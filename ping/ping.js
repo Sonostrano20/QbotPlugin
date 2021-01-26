e1794b49b10388e8f1f6d7caff5fe358192481fb
Sonome20 <55855409+Sonostrano20@users.noreply.github.com>
2021-01-26 17:35:08 +0100

    Update ping.js

--- ping/ping.js
+++ ping/ping.js
@@ -1,4 +1,23 @@
-exports.run = async (client, message, args) => {
-    const pingMessage = await message.channel.send("🏓 **Ping?**");
-    pingMessage.edit(`Pong! Latency is ${pingMessage.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
-  }
+const Discord = require('discord.js');
+
+const config = {
+    description: 'Pinga il bot per sapere quanto ritardo ha su discord.',
+    aliases: [],
+    rolesRequired: [],
+    category: '<:3224_info:790237145574014997> Informazioni',
+    cooldown: 5,
+}
+module.exports = {
+    config,
+    run: async (client, message, args) => {
+
+if(!message.channel.guild) return message.channel.send(':x: This command should only be used on the server. ');
+
+    const ping = new Discord.MessageEmbed()
+    .setColor('RANDOM')
+    .setTitle('Ping')
+    .setDescription("My Ping: `" + `${Date.now() - message.createdTimestamp}` + " ms`")
+    .setFooter(`Required by: ${message.author.username}`);
+     message.channel.send(ping);
+   },
+};
